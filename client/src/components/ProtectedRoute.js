363e56f3c1116fe72c6f3eb6abc48f3c8dc72000
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-10-16 08:37:08 +0530

    Add protected route

---
+++ client/src/components/ProtectedRoute.js
@@ -0,0 +1,10 @@
+// ProtectedRoute.js
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }) => {
+    const isAuthenticated = localStorage.getItem("TOKEN") !== null;
+    
+    return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
