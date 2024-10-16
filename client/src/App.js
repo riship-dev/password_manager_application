235c76ddc371e0bbd64257ce1fe3efcf59a52e79
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-10-16 08:04:50 +0530

    Add pages to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
-const App = () => {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
     return (
-        <div className="App"></div>
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+            </Routes>
+        </Router>
     );
 }
 
