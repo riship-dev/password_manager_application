5a0290417ae90c657781293a684ed916329534a2
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-10-16 05:52:48 +0530

    Update server.js to use passwords routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import DB from "./models/database.js";
 import authenticationRoutes from "./routes/authentication.js";
-//import passwordRoutes from "./routes/passwords.js";
+import passwordRoutes from "./routes/passwords.js";
 dotenv.config();
 DB.connect()
     .then(() => {
@@ -19,7 +19,7 @@ const PORT = 5000;
 APP.use(cors());
 APP.use(express.json());
 APP.use("/api/authentication", authenticationRoutes);
-//APP.use("/api/passwords", passwordRoutes);
+APP.use("/api/passwords", passwordRoutes);
 
 APP.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
