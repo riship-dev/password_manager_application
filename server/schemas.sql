3cc244270b119bbbc74c5e26dadf8740e7e12fac
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-10-18 03:27:19 +0530

    Alter service_password column data type to TEXT to store encrypted passwords

--- server/schemas.sql
+++ server/schemas.sql
@@ -11,7 +11,7 @@ CREATE TABLE passwords (
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     service_name VARCHAR(100) NOT NULL,
     service_username VARCHAR(100) NOT NULL,
-    service_password VARCHAR(255) NOT NULL,
+    service_password TEXT NOT NULL,
     notes TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
